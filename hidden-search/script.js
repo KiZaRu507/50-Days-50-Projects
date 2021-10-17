6108c0ad11093c03266cef85d0c5811b11d9d0a8
Sumukh VR <vrsumukh32@gmail.com>
2021-10-17 11:36:43 +0530

    Final changes done

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -1,10 +1,19 @@
 var button=document.querySelector('button');
-var box=document.querySelector('.input');
+
+var box=document.querySelector('.container');
 button.addEventListener('click',()=>{
-    if(box.classList.contains('hidden')){
-        box.classList.remove('hidden');
+    if(box.classList.contains('active')){
+        box.classList.remove('active');
     }
     else{
-        box.classList.add('hidden');
+        box.classList.add('active');
+    }
+})
+document.addEventListener('keydown',()=>{
+    if(box.classList.contains('active')){
+        box.classList.remove('active');
+    }
+    else{
+        box.classList.add('active');
     }
 })
